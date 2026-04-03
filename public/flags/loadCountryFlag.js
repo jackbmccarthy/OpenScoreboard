95db8141a14699456ef812bd3940e6acd74540b0
Lauren (AI Assistant) <lauren@openscoreboard.local>
2026-04-02 17:30:19 -0700

    fix: restore public/flags for country flag images
    Copied from old openscoreboard-scoreboard:
    - public/flags/countries.json
    - public/flags/loadCountryFlag.js
    
    These are needed for country flags in scoreboards.

---
+++ public/flags/loadCountryFlag.js
@@ -0,0 +1,515 @@
+
+
+export async function loadFlag(countryCode) {
+    let imageData = await require(`./${countryCode}.png`)
+    console.log(imageData)
+    return imageData
+}
+import AD from './ad.png'
+import AE from './ae.png'
+import AF from './af.png'
+import AG from './ag.png'
+import AI from './ai.png'
+import AL from './al.png'
+import AM from './am.png'
+import AO from './ao.png'
+import AQ from './aq.png'
+import AR from './ar.png'
+import AS from './as.png'
+import AT from './at.png'
+import AU from './au.png'
+import AW from './aw.png'
+import AX from './ax.png'
+import AZ from './az.png'
+import BA from './ba.png'
+import BB from './bb.png'
+import BD from './bd.png'
+import BE from './be.png'
+import BF from './bf.png'
+import BG from './bg.png'
+import BH from './bh.png'
+import BI from './bi.png'
+import BJ from './bj.png'
+import BL from './bl.png'
+import BM from './bm.png'
+import BN from './bn.png'
+import BO from './bo.png'
+import BQ from './bq.png'
+import BR from './br.png'
+import BS from './bs.png'
+import BT from './bt.png'
+import BV from './bv.png'
+import BW from './bw.png'
+import BY from './by.png'
+import BZ from './bz.png'
+import CA from './ca.png'
+import CC from './cc.png'
+import CD from './cd.png'
+import CF from './cf.png'
+import CG from './cg.png'
+import CH from './ch.png'
+import CI from './ci.png'
+import CK from './ck.png'
+import CL from './cl.png'
+import CM from './cm.png'
+import CN from './cn.png'
+import CNT from './cnt.png'
+import CO from './co.png'
+import CR from './cr.png'
+import CU from './cu.png'
+import CV from './cv.png'
+import CW from './cw.png'
+import CX from './cx.png'
+import CY from './cy.png'
+import CZ from './cz.png'
+import DE from './de.png'
+import DJ from './dj.png'
+import DK from './dk.png'
+import DM from './dm.png'
+import DO from './do.png'
+import DZ from './dz.png'
+import EC from './ec.png'
+import EE from './ee.png'
+import EG from './eg.png'
+import EH from './eh.png'
+import ER from './er.png'
+import ES from './es.png'
+import ET from './et.png'
+import EU from './eu.png'
+import FI from './fi.png'
+import FJ from './fj.png'
+import FK from './fk.png'
+import FM from './fm.png'
+import FO from './fo.png'
+import FR from './fr.png'
+import GA from './ga.png'
+import GB from './gb.png'
+import GD from './gd.png'
+import GE from './ge.png'
+import GF from './gf.png'
+import GG from './gg.png'
+import GH from './gh.png'
+import GI from './gi.png'
+import GL from './gl.png'
+import GM from './gm.png'
+import GN from './gn.png'
+import GP from './gp.png'
+import GQ from './gq.png'
+import GR from './gr.png'
+import GS from './gs.png'
+import GT from './gt.png'
+import GU from './gu.png'
+import GW from './gw.png'
+import GY from './gy.png'
+import HK from './hk.png'
+import HM from './hm.png'
+import HN from './hn.png'
+import HR from './hr.png'
+import HT from './ht.png'
+import HU from './hu.png'
+import ID from './id.png'
+import IE from './ie.png'
+import IL from './il.png'
+import IM from './im.png'
+import IN from './in.png'
+import IO from './io.png'
+import IQ from './iq.png'
+import IR from './ir.png'
+import IS from './is.png'
+import IT from './it.png'
+import JE from './je.png'
+import JM from './jm.png'
+import JO from './jo.png'
+import JP from './jp.png'
+import KE from './ke.png'
+import KG from './kg.png'
+import KH from './kh.png'
+import KI from './ki.png'
+import KM from './km.png'
+import KN from './kn.png'
+import KP from './kp.png'
+import KR from './kr.png'
+import KW from './kw.png'
+import KY from './ky.png'
+import KZ from './kz.png'
+import LA from './la.png'
+import LB from './lb.png'
+import LC from './lc.png'
+import LI from './li.png'
+import LK from './lk.png'
+import LR from './lr.png'
+import LS from './ls.png'
+import LT from './lt.png'
+import LU from './lu.png'
+import LV from './lv.png'
+import LY from './ly.png'
+import MA from './ma.png'
+import MC from './mc.png'
+import MD from './md.png'
+import ME from './me.png'
+import MF from './mf.png'
+import MG from './mg.png'
+import MH from './mh.png'
+import MK from './mk.png'
+import ML from './ml.png'
+import MM from './mm.png'
+import MN from './mn.png'
+import MO from './mo.png'
+import MP from './mp.png'
+import MQ from './mq.png'
+import MR from './mr.png'
+import MS from './ms.png'
+import MT from './mt.png'
+import MU from './mu.png'
+import MV from './mv.png'
+import MW from './mw.png'
+import MX from './mx.png'
+import MY from './my.png'
+import MZ from './mz.png'
+import NA from './na.png'
+import NC from './nc.png'
+import NE from './ne.png'
+import NF from './nf.png'
+import NG from './ng.png'
+import NI from './ni.png'
+import NL from './nl.png'
+import NO from './no.png'
+import NP from './np.png'
+import NR from './nr.png'
+import NU from './nu.png'
+import NZ from './nz.png'
+import OM from './om.png'
+import PA from './pa.png'
+import PE from './pe.png'
+import PF from './pf.png'
+import PG from './pg.png'
+import PH from './ph.png'
+import PK from './pk.png'
+import PL from './pl.png'
+import PM from './pm.png'
+import PN from './pn.png'
+import PR from './pr.png'
+import PS from './ps.png'
+import PT from './pt.png'
+import PW from './pw.png'
+import PY from './py.png'
+import QA from './qa.png'
+import RE from './re.png'
+import RO from './ro.png'
+import RS from './rs.png'
+import RU from './ru.png'
+import RW from './rw.png'
+import SA from './sa.png'
+import SB from './sb.png'
+import SC from './sc.png'
+import SD from './sd.png'
+import SE from './se.png'
+import SG from './sg.png'
+import SH from './sh.png'
+import SI from './si.png'
+import SJ from './sj.png'
+import SK from './sk.png'
+import SL from './sl.png'
+import SM from './sm.png'
+import SN from './sn.png'
+import SO from './so.png'
+import SR from './sr.png'
+import SS from './ss.png'
+import ST from './st.png'
+import SV from './sv.png'
+import SX from './sx.png'
+import SY from './sy.png'
+import SZ from './sz.png'
+import TC from './tc.png'
+import TD from './td.png'
+import TF from './tf.png'
+import TG from './tg.png'
+import TH from './th.png'
+import TJ from './tj.png'
+import TK from './tk.png'
+import TL from './tl.png'
+import TM from './tm.png'
+import TN from './tn.png'
+import TO from './to.png'
+import TR from './tr.png'
+import TT from './tt.png'
+import TV from './tv.png'
+import TW from './tw.png'
+import TZ from './tz.png'
+import UA from './ua.png'
+import UG from './ug.png'
+import UM from './um.png'
+import US from './us.png'
+import UY from './uy.png'
+import UZ from './uz.png'
+import VA from './va.png'
+import VC from './vc.png'
+import VE from './ve.png'
+import VG from './vg.png'
+import VI from './vi.png'
+import VN from './vn.png'
+import VU from './vu.png'
+import WF from './wf.png'
+import WS from './ws.png'
+import XK from './xk.png'
+import YE from './ye.png'
+import YT from './yt.png'
+import ZA from './za.png'
+import ZM from './zm.png'
+import ZW from './zw.png'
+
+
+export const flagData = {
+    "AD": AD,
+    "AE": AE,
+    "AF": AF,
+    "AG": AG,
+    "AI": AI,
+    "AL": AL,
+    "AM": AM,
+    "AO": AO,
+    "AQ": AQ,
+    "AR": AR,
+    "AS": AS,
+    "AT": AT,
+    "AU": AU,
+    "AW": AW,
+    "AX": AX,
+    "AZ": AZ,
+    "BA": BA,
+    "BB": BB,
+    "BD": BD,
+    "BE": BE,
+    "BF": BF,
+    "BG": BG,
+    "BH": BH,
+    "BI": BI,
+    "BJ": BJ,
+    "BL": BL,
+    "BM": BM,
+    "BN": BN,
+    "BO": BO,
+    "BQ": BQ,
+    "BR": BR,
+    "BS": BS,
+    "BT": BT,
+    "BV": BV,
+    "BW": BW,
+    "BY": BY,
+    "BZ": BZ,
+    "CA": CA,
+    "CC": CC,
+    "CD": CD,
+    "CF": CF,
+    "CG": CG,
+    "CH": CH,
+    "CI": CI,
+    "CK": CK,
+    "CL": CL,
+    "CM": CM,
+    "CN": CN,
+    "CNT": CNT,
+    "CO": CO,
+    "CR": CR,
+    "CU": CU,
+    "CV": CV,
+    "CW": CW,
+    "CX": CX,
+    "CY": CY,
+    "CZ": CZ,
+    "DE": DE,
+    "DJ": DJ,
+    "DK": DK,
+    "DM": DM,
+    "DO": DO,
+    "DZ": DZ,
+    "EC": EC,
+    "EE": EE,
+    "EG": EG,
+    "EH": EH,
+    "ER": ER,
+    "ES": ES,
+    "ET": ET,
+    "EU": EU,
+    "FI": FI,
+    "FJ": FJ,
+    "FK": FK,
+    "FM": FM,
+    "FO": FO,
+    "FR": FR,
+    "GA": GA,
+    "GB": GB,
+    "GD": GD,
+    "GE": GE,
+    "GF": GF,
+    "GG": GG,
+    "GH": GH,
+    "GI": GI,
+    "GL": GL,
+    "GM": GM,
+    "GN": GN,
+    "GP": GP,
+    "GQ": GQ,
+    "GR": GR,
+    "GS": GS,
+    "GT": GT,
+    "GU": GU,
+    "GW": GW,
+    "GY": GY,
+    "HK": HK,
+    "HM": HM,
+    "HN": HN,
+    "HR": HR,
+    "HT": HT,
+    "HU": HU,
+    "ID": ID,
+    "IE": IE,
+    "IL": IL,
+    "IM": IM,
+    "IN": IN,
+    "IO": IO,
+    "IQ": IQ,
+    "IR": IR,
+    "IS": IS,
+    "IT": IT,
+    "JE": JE,
+    "JM": JM,
+    "JO": JO,
+    "JP": JP,
+    "KE": KE,
+    "KG": KG,
+    "KH": KH,
+    "KI": KI,
+    "KM": KM,
+    "KN": KN,
+    "KP": KP,
+    "KR": KR,
+    "KW": KW,
+    "KY": KY,
+    "KZ": KZ,
+    "LA": LA,
+    "LB": LB,
+    "LC": LC,
+    "LI": LI,
+    "LK": LK,
+    "LR": LR,
+    "LS": LS,
+    "LT": LT,
+    "LU": LU,
+    "LV": LV,
+    "LY": LY,
+    "MA": MA,
+    "MC": MC,
+    "MD": MD,
+    "ME": ME,
+    "MF": MF,
+    "MG": MG,
+    "MH": MH,
+    "MK": MK,
+    "ML": ML,
+    "MM": MM,
+    "MN": MN,
+    "MO": MO,
+    "MP": MP,
+    "MQ": MQ,
+    "MR": MR,
+    "MS": MS,
+    "MT": MT,
+    "MU": MU,
+    "MV": MV,
+    "MW": MW,
+    "MX": MX,
+    "MY": MY,
+    "MZ": MZ,
+    "NA": NA,
+    "NC": NC,
+    "NE": NE,
+    "NF": NF,
+    "NG": NG,
+    "NI": NI,
+    "NL": NL,
+    "NO": NO,
+    "NP": NP,
+    "NR": NR,
+    "NU": NU,
+    "NZ": NZ,
+    "OM": OM,
+    "PA": PA,
+    "PE": PE,
+    "PF": PF,
+    "PG": PG,
+    "PH": PH,
+    "PK": PK,
+    "PL": PL,
+    "PM": PM,
+    "PN": PN,
+    "PR": PR,
+    "PS": PS,
+    "PT": PT,
+    "PW": PW,
+    "PY": PY,
+    "QA": QA,
+    "RE": RE,
+    "RO": RO,
+    "RS": RS,
+    "RU": RU,
+    "RW": RW,
+    "SA": SA,
+    "SB": SB,
+    "SC": SC,
+    "SD": SD,
+    "SE": SE,
+    "SG": SG,
+    "SH": SH,
+    "SI": SI,
+    "SJ": SJ,
+    "SK": SK,
+    "SL": SL,
+    "SM": SM,
+    "SN": SN,
+    "SO": SO,
+    "SR": SR,
+    "SS": SS,
+    "ST": ST,
+    "SV": SV,
+    "SX": SX,
+    "SY": SY,
+    "SZ": SZ,
+    "TC": TC,
+    "TD": TD,
+    "TF": TF,
+    "TG": TG,
+    "TH": TH,
+    "TJ": TJ,
+    "TK": TK,
+    "TL": TL,
+    "TM": TM,
+    "TN": TN,
+    "TO": TO,
+    "TR": TR,
+    "TT": TT,
+    "TV": TV,
+    "TW": TW,
+    "TZ": TZ,
+    "UA": UA,
+    "UG": UG,
+    "UM": UM,
+    "US": US,
+    "UY": UY,
+    "UZ": UZ,
+    "VA": VA,
+    "VC": VC,
+    "VE": VE,
+    "VG": VG,
+    "VI": VI,
+    "VN": VN,
+    "VU": VU,
+    "WF": WF,
+    "WS": WS,
+    "XK": XK,
+    "YE": YE,
+    "YT": YT,
+    "ZA": ZA,
+    "ZM": ZM,
+    "ZW": ZW,
+}
